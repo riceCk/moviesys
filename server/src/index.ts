029f626aef8e81f847b23083ee49d398c53407a1
riceCk <xiehb@tydic.com>
2020-08-28 15:21:02 +0800

    2020/08/28 临时提交

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,11 @@
 import 'reflect-metadata';
-import { MovieService } from './services/MovieService';
-import { MovieModel } from './db';
-import { Movie } from './entities/Movie';
+import Express from 'express';
+import MovieRouter from './routers/MovieRoute';
 
-const condi: any = {
-  page: 10,
-  limit: 5,
-  key: ''
-}
-MovieService.find(condi).then(result => {
-  if (result.errors.length > 0) {
-    console.log(result.errors)
-  } else {
-    result.data.forEach(item => console.log(item.name))
-    console.log(result.count)
-  }
+const app = Express()
+
+app.use("/api/movie", MovieRouter)
+
+app.listen(3000, () => {
+  console.log('开启服务器')
 })
-
-// function getRandom(min: number, max: number) {
-//   const dec = max - min;
-//   return Math.floor(Math.random() * dec + min);
-// }
-
-// for (let i = 0; i < 100; i++) {
-//   const m = new Movie()
-//   m.name = '电影' + (i + 1);
-//   m.areas = ['中国大陆', '美国']
-//   m.type = ["喜剧", "动作"]
-//   m.isClassic = true;
-//   m.timeLong = getRandom(70, 240);
-//   MovieService.add(m);
-// }
